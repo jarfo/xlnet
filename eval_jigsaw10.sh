7012072e492075b3c948c45ee966e3165bb9da2f
jarfo <jarfo@yahoo.com>
2019-07-22 17:24:47 +0000

    *.sh

--- eval_jigsaw10.sh
+++ eval_jigsaw10.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-export TPU_NAME=xlnet1
+export TPU_NAME=xlnet3
 export JIGSAW_DIR=../input/jigsaw-unintended-bias-in-toxicity-classification
 export GS_ROOT=gs://tpubert
 export LARGE_DIR=models/xlnet_cased_L-24_H-1024_A-16
 
 python run_jigsaw.py \
+  --learning_rate=1e-5 \
   --use_tpu=True \
   --tpu=${TPU_NAME} \
-  --do_train=True \
+  --do_train=False \
   --do_eval=True \
-  --eval_all_ckpt=True \
+  --eval_all_ckpt=False \
   --task_name=jigsaw \
   --data_dir=${JIGSAW_DIR} \
-  --output_dir=${GS_ROOT}/xlnet1/proc_data/jigsaw \
-  --model_dir=${GS_ROOT}/xlnet1/exp/jigsaw \
+  --output_dir=${GS_ROOT}/xlnet10/proc_data/jigsaw \
+  --model_dir=${GS_ROOT}/xlnet10/exp/jigsaw10 \
   --uncased=False \
   --spiece_model_file=${LARGE_DIR}/spiece.model \
   --model_config_path=${GS_ROOT}/${LARGE_DIR}/xlnet_config.json \
   --init_checkpoint=${GS_ROOT}/${LARGE_DIR}/xlnet_model.ckpt \
   --max_seq_length=512 \
   --train_batch_size=32 \
-  --eval_batch_size=8 \
+  --eval_batch_size=80 \
   --num_hosts=1 \
   --num_core_per_host=8 \
-  --learning_rate=2e-5 \
   --train_steps=200000 \
   --warmup_steps=25000 \
-  --save_steps=500 \
-  --iterations=500
+  --save_steps=5000 \
+  --iterations=5000
